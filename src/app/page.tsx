5341c5bf4285a50286ab9ec96918af18462afdce
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-11-08 20:41:50 +0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 import BottomNav from './components/BottomNav';
 
 export default function Home() {
+  const bottomNavHeight = 75; // tinggi nav bar (px)
+
   return (
     <>
       <main
-        className="flex flex-col items-center justify-center px-6 py-16 bg-cover bg-center text-center font-sans text-white"
+        className="flex flex-col items-center justify-center bg-center text-center font-sans text-white"
         style={{
           backgroundImage: "url('/images/1.jpg')",
-          height: "calc(100vh - 75px)", // kurangi tinggi bottom nav
+          backgroundRepeat: "no-repeat",
+          minHeight: `calc(100vh - ${bottomNavHeight}px)`, // tinggi menyesuaikan nav
         }}
       >
       </main>
+
+      {/* <div style={{ height: `${bottomNavHeight}px` }} />  */}
+      {/* spacer agar konten tidak ketutupan bottom nav */}
+
       <BottomNav />
     </>
   );
